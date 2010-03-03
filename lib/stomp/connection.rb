aa1e5e546a6413a0c25f6165c96bf8d22ee59c59
Thiago Morello <morello@queroinfra32.fabrica.locaweb.com.br>
2010-03-03 12:13:38 -0300

    If used supress_content_length header, send it as header for future handling using unreceive

--- lib/stomp/connection.rb
+++ lib/stomp/connection.rb
@@ -406,8 +406,8 @@ module Stomp
           # Using :suppress_content_length => true will suppress this behaviour
           # and ActiveMQ will interpret the message as a TextMessage.
           # For more information refer to http://juretta.com/log/2009/05/24/activemq-jms-stomp/
-          suppress_content_length = headers.delete :suppress_content_length
-          headers['content-length'] = "#{body.length}" unless suppress_content_length
+          # Lets send this header in the message, so it can maintain state when using unreceive
+          headers['content-length'] = "#{body.length}" unless headers[:suppress_content_length]
           
           used_socket.puts command  
           headers.each {|k,v| used_socket.puts "#{k}:#{v}" }
